5175d41a7aa5ecd039aee4824e5c731e1b3f6f24
AndriiTyschuk <andrii.tyschuk@ithoot.com>
2026-03-26 20:51:09 +0200

    fix: app bar buttons fitting

--- lib/common/widgets/theme_switch.dart
+++ lib/common/widgets/theme_switch.dart
@@ -1,23 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:trip_assistant/common/styles/styles.dart';
 import 'package:trip_assistant/utils/theme/controller.dart';
 
 class ThemeSwitch extends StatelessWidget{
   const ThemeSwitch({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Consumer<ThemeController>(
-      builder: (context, controller, _) {
-        return IconButton(
-          icon: Icon(
-            controller.isDark 
-              ? Icons.dark_mode 
-              : Icons.light_mode
-          ),
-          onPressed: controller.toggleTheme,
-        );
-      },
-    );
-  }
+  Widget build(BuildContext context) => Consumer<ThemeController>(
+    builder: (context, controller, _) => Padding(
+      padding: EdgeInsets.symmetric(
+        vertical: 0, 
+        horizontal: BlockProperties.thinPadding
+      ), 
+      child: IconButton(
+        icon: Icon(
+          controller.isDark 
+            ? Icons.dark_mode 
+            : Icons.light_mode
+        ),
+        onPressed: controller.toggleTheme,
+      ),
+    ),
+  );
 }
